f26a90136370e1ec28a968449c8facf0462b0a3a
Nestor Cervantes <ncervan87@gmail.com>
2024-10-15 00:55:28 -0500

    add devlopment environment

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,12 @@
-require('dotenv').config();
+const dotenv = require('dotenv');
+
+const envFile = process.env.NODE_ENV === 'production'
+    ? '.env.production'
+    : process.env.NODE_ENV === 'test'
+        ? '.env.test'
+        : '.env.development';
+
+dotenv.config({ path: envFile });
 
 const express = require('express');
 const cors = require('cors');
