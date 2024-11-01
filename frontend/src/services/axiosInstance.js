fdf780f113dc7007c5f9f2d5287485ea97c36e75
Nestor Cervantes <ncervan87@gmail.com>
2024-11-01 17:46:48 -0500

    update axios config

--- frontend/src/services/axiosInstance.js
+++ frontend/src/services/axiosInstance.js
@@ -1,9 +1,8 @@
 import axios from 'axios';
 
-const apiBaseUrl = window.location.hostname === 'localhost'
+const apiBaseUrl = import.meta.env.MODE === 'development'
   ? import.meta.env.VITE_BACKEND_LOCALHOST
-  : import.meta.env.VITE_BACKEND_LOCAL_IP;
-
+  : import.meta.env.VITE_BACKEND_PRODUCTION;
 
 const axiosInstance = axios.create({
   baseURL: apiBaseUrl,
